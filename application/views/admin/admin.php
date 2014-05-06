a49467bdb9bb81ea9900b87c6e2b7611e5a1a583
Weldeasy <vivaelcole@gmail.com>
2014-05-06 19:44:56 +0200

    test2

--- application/views/admin/admin.php
+++ application/views/admin/admin.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Dashboard - SB Admin</title>
+    <title>Dashboard - SB AdminMAQUINOTEEEEEEEEEEEEEEE</title>
 
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url();?>media/css/bootstrap.css" rel="stylesheet">
