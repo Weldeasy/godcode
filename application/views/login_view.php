70d83637b66fb74f441da7db614430eb6f1c180d
wills1994 <wills1994@hotmail.com>
2014-05-07 16:53:13 +0200

    dd

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title>Simple Login with CodeIgniter wilson</title>
+    <title>Simple Login with CodeIgniter</title>
   </head>
   <body>
-    <h1>Simple 2 Login with CodeIgniter-Anton</h1>
+    <h1>Simple 2 Login with CodeIgniter-Anton wilson</h1>
     <?php echo validation_errors()."hOLA"; ?>
     <?php echo form_open('verifylogin'); ?>
       <label for="username">Username:</label>
