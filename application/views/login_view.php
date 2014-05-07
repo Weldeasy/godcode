1e34713e210ba661420dcc5350050920ee480cce
wills1994 <wills1994@hotmail.com>
2014-05-07 16:28:19 +0200

    CANVI HOLA

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -5,7 +5,7 @@
   </head>
   <body>
     <h1>Simple Login with CodeIgniter</h1>
-    <?php echo validation_errors(); ?>
+    <?php echo validation_errors()."hOLA"; ?>
     <?php echo form_open('verifylogin'); ?>
       <label for="username">Username:</label>
       <input type="text" size="20" id="username" name="username"/>
