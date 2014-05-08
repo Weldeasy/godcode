02d25be6a76d87f581ab76dfef437430fb625859
wills1994 <wills1994@hotmail.com>
2014-05-08 17:31:14 +0200

    ghg
    hh

--- application/views/template.php
+++ application/views/template.php
@@ -60,7 +60,7 @@
     </ul>
 <a href="http://mambocar.com/publicar-coche/registro" id="bt_como_publicar_tu_coche">Publica tu coche</a>            </div>
 
-<a href="./admin/admin.php" id="bt_como_publicar_tu_coche">Admin</a></div>
+<a href='<?php echo site_url('admin/admin.php')?>' id="bt_como_publicar_tu_coche">Admin</a></div>
           </div>
         </div>
         <div class="parte_derecha"></div>
