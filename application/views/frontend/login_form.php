21718e0cc79be4ac85213167018ef5b8158b98d5
Weldeasy <vivaelcole@gmail.com>
2014-05-10 01:06:50 +0200

    aa

--- application/views/frontend/login_form.php
+++ application/views/frontend/login_form.php
@@ -1,10 +1,10 @@
 <?php
 
-//$attributes = array('class' => 'email', 'id' => 'form_login');
-//echo form_open('verifylogin', $attributes);
+$attributes = array('class' => 'email', 'id' => 'form_login');
+echo form_open('verifylogin', $attributes);
 //if (isset(validation_errors()) {echo validation_errors();}
 ?>
-<form action="verifylogin" class="email" id="form_login">
+
 	<label for="username">Username:</label>
 	<input type="text" size="20" id="username" name="username"/>
 	<br/>
