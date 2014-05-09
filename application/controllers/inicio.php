3028265fc46099d8c16956bbd3a392c9e8733738
Weldeasy <vivaelcole@gmail.com>
2014-05-10 00:49:37 +0200

    aa

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -9,6 +9,7 @@ class Inicio extends CI_Controller {
 
   function index()
   {
+	$data = new Array();
 	$data['login_form'] = $this->load->view('login-form');
     $this->load->view('inicio', $data);
   }
