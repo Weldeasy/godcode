dfcd4e142ef8703b570a4e19551b805556338c90
Weldeasy <vivaelcole@gmail.com>
2014-05-10 01:55:04 +0200

    aa

--- application/views/frontend/login_form.php
+++ application/views/frontend/login_form.php
@@ -3,11 +3,12 @@
 //$attributes = array('class' => 'email', 'id' => 'form_login');
 //echo form_open('verifylogin', $attributes); 
 ?>
-	<label for="username">Username:</label>
+	<!--<label for="username">Username:</label>
 	<input type="text" size="20" id="username" name="username"/>
 	<br/>
 	<label for="password">Password:</label>
 	<input type="password" size="20" id="passowrd" name="password"/>
 	<br/>
 	<input type="submit" value="Login"/>
-</form>
+</form>-->
+<a href="a">xDD</a>
