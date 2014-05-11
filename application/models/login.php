995ea6ba8901595a7604f2ff47c974cac840b1ea
Weldeasy <vivaelcole@gmail.com>
2014-05-11 21:16:55 +0200

    aa

--- application/models/login.php
+++ application/models/login.php
@@ -3,11 +3,11 @@ Class Login extends CI_Model
 {
 	function login($email, $password)
 	{
-		$this -> db -> select('usuari.id, login.email, login.passwd, es_admin');
+		$this -> db -> select('usuari.id, login.email, login.password, es_admin');
 		$this -> db -> from('login');
 		$this -> db -> from('usuari');
 		$this -> db -> where('login.email = ' . "'" . $email . "'"); 
-		$this -> db -> where('login.passwd = ' . "'" . MD5($password) . "'"); 
+		$this -> db -> where('login.password = ' . "'" . MD5($password) . "'"); 
 		$this -> db -> where('login.id = usuari.id'); 
 		
 		$this -> db -> limit(1);
