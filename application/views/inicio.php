e1a77684d7d36347feabdc9bf32f4fd29a9d6124
amartintapia <alejandromartintapia@gmail.com>
2014-05-12 17:30:53 +0200

    w

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -7,7 +7,7 @@
     <meta name="description" content="" />
     <meta name="keywords" content="" />
     <meta name="author" content="">
-    <link href="<?= base_url(); ?>media/css/style.css" rel="stylesheet">
+    <link href="<?= base_url();?> /media/css/style.css" rel="stylesheet">
     <link href="navbar-fixed-top.css" rel="stylesheet">
 <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
   </head>
