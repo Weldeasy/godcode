c61d052f53d8f03a4d6ed1e94fcefbad09773b2f
Weldeasy <vivaelcole@gmail.com>
2014-05-12 16:24:36 +0200

    aa

--- application/controllers/FormularioRegistro.php
+++ application/controllers/FormularioRegistro.php
@@ -42,7 +42,7 @@ class Formularioregistro extends CI_Controller {
 		$data['poblacions'] = $this->poblacions->get_poblacions();
 		
 		$this->load->view('frontend/registro_nuevo_usuario', $data);*/
-		$this->load->view('frontend/registro_nuevo_usuario');
+		echo "aa"; //$this->load->view('frontend/registro_nuevo_usuario');
 	}
 	
 	
