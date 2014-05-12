e95ca5a3b4c295ba8390522bd5bb3bb048b4329d
Weldeasy <vivaelcole@gmail.com>
2014-05-12 15:21:25 +0200

    aa

--- application/controllers/aboutus.php
+++ application/controllers/aboutus.php
@@ -19,7 +19,18 @@ class Aboutus extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('frontend/aboutus');
+		$data = array();
+		$this->load->library('form_validation');
+		$this->load->helper(array('form'));
+		if($this->session->userdata('logged_in')) {
+			$session_data = $this->session->userdata('logged_in');
+			$data['email'] = $session_data['email'];
+			$data['login_form'] = 'frontend/logued';
+		} else {
+			$data['login_form'] = 'frontend/login_form';
+		}
+
+		$this->load->view('frontend/aboutus', $data);
 	}
 }
 
