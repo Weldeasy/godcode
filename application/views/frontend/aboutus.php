faed2c0ab9a2fb96dac4e69c3eea3c7cd73a7a07
amartintapia <alejandromartintapia@gmail.com>
2014-05-14 17:52:50 +0200

    aboutus

--- application/views/frontend/aboutus.php
+++ application/views/frontend/aboutus.php
@@ -30,26 +30,12 @@
 ?>
   </ul>
  </div>
- <div id="busqueda">
- <div class="seccion zona_busqueda">
-      <center>
-        <div class="contenido_zona_busqueda">
-            <div class="caja_busqueda">
+ <div id="contenido_principal">
+  <div id="about_us">
               <h1>SOBRE NOSALTRES</h1>
-              <h3>Encuentra los servicios que hay disponibles en tu zona</h3>
-              <hr>
-              <br>
-              <div class="contenido_formulario_busqueda">
-                <form id="landingForm" action="" method="post">
-                    <input id="city_id" type="hidden" value="0">
-                    <input id="city_name" type="hidden" value="">
-
-                    <span role="status" aria-live="polite" class="ui-helper-hidden-accessible"></span><input id="autocomplete_city" name="autocomplete_city" class="input_ciudad ui-autocomplete-input" type="text" placeholder="Código postal ciudad: " required="" title="Por favor selecciona una ciudad" autofocus="" autocomplete="off">
-                    <input id="date_from" class="input_fecha hasDatepicker" type="text" name="date_from" placeholder="" autocomplete="off" required="" title="Selecciona la fecha inicial de la reserva">
-                    <input id="date_to" class="input_fecha hasDatepicker" type="text" name="date_to" placeholder="" autocomplete="off" required="" title="Selecciona la fecha final de la reserva">
-                    <input type="submit" value="Buscar servicios" class="bt_ver_coches">
-                </form>
-              </div>
+              <hr />
+              <br />
+              <p>Godcode és una empresa impulsada per un jove grup de programadors web. Malgrat ser un grup tan jove, podem encarrilar qualsevol projecte gràcies a les nostres ganes i professionalitat. Tenim un gran ventall de projectes nostres, si vulgues més informació posi's en contacte amb nosaltres o miri en la nostra web.</p>
             </div>
         </div>
       </center>
