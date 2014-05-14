64af7303a008a6e528340a4d5978d8718710f03b
amartintapia <alejandromartintapia@gmail.com>
2014-05-14 18:36:06 +0200

    b

--- application/views/frontend/aboutus.php
+++ application/views/frontend/aboutus.php
@@ -36,6 +36,34 @@
               <hr />
               <br />
               <p>Godcode és una empresa impulsada per un jove grup de programadors web. Malgrat ser un grup tan jove, podem encarrilar qualsevol projecte gràcies a les nostres ganes i professionalitat. Tenim un gran ventall de projectes nostres, si vulgues més informació posi's en contacte amb nosaltres o miri en la nostra web.</p>
+              <table id="table_programadors">
+                <td>
+                  <tr>
+                    <p>Alex Martín Tapia</p>
+                    <ul>
+                      <li>Programador informátic</li>
+                    </ul>
+                  </tr>
+                  <tr>
+                    <p>Oscar Marcos</p>
+                    <ul>
+                      <li>Programador informátic</li>
+                    </ul>
+                  </tr>
+                  <tr>
+                    <p>Oriol Antón</p>
+                    <ul>
+                      <li>Programador informátic</li>
+                    </ul>
+                  </tr>
+                  <tr>
+                    <p>Willy</p>
+                    <ul>
+                      <li>Programador informátic</li>
+                    </ul>
+                  </tr>
+                </td>
+              </table>
             </div>
         </div>
       </center>
