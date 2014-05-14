0056947d82f80b811ecca2db26c745b392be27ce
amartintapia <alejandromartintapia@gmail.com>
2014-05-14 19:04:38 +0200

    prueba

---
+++ application/views/frontend/php/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="es" xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="">
+    <link href="<?= base_url()?>media/css/style.css" rel="stylesheet">
+    <link href="navbar-fixed-top.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+    <link href='<?= base_url()?>media/css/login.css' rel='stylesheet' type='text/css'>
+  </head>
