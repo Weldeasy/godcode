d00838dc464a43b2270adcc882bf43ef002d7905
Weldeasy <vivaelcole@gmail.com>
2014-05-15 19:04:22 +0200

    aa

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -10,7 +10,8 @@ class Admin extends CI_Controller {
     	parent::__construct();
 		$this->load->database();
 		$this->load->model('adm','',TRUE);
-		//$session_data = $this->session->userdata('logged_in');
+		$session_data = array();
+		$session_data = $this->session->userdata('logged_in');
 	}
 
 
@@ -24,7 +25,7 @@ class Admin extends CI_Controller {
 		$estat = $this->session->userdata('estat');
 		if($this->session->userdata('logged_in') && $estat==2) {
 			
-			$data['email'] = $this->session->userdata('logged_in');
+			$data['email'] = $this->session_data['email'];
 			$data['panel_admin']=$this->load->view('backend/pages/panel_admin', null, TRUE);
 			$this->load->view('backend/admin',$data);
 		} else {
