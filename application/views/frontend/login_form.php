cc842c49de9348ed1c88b45c7b07016125dc145f
oaoanton <oaoanton@gmail.com>
2014-05-15 21:27:11 +0200

    fgh

--- application/views/frontend/login_form.php
+++ application/views/frontend/login_form.php
@@ -4,5 +4,5 @@ echo form_open('verifylogin',array('class'=>'login_form'));
 ?>
 	<input class="user_opt" type="text" value="<?= @set_value("email")?>" size="20" id="email" name="email"/>
 	<input class="user_opt_p" type="password" size="20" id="password" name="password"/>
-	<input type="submit" value="Login"/>
+	<input type="submit" value="ENTRAR" title="Accedir a la meva conte" />
 </form>
