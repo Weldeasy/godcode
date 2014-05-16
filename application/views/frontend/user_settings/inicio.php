33adbd9dad0c936cd7c3567a2c40dba969726cdf
oaoanton <oaoanton@gmail.com>
2014-05-16 11:13:49 +0200

    s

--- application/views/frontend/user_settings/inicio.php
+++ application/views/frontend/user_settings/inicio.php
@@ -18,14 +18,16 @@
 			<div class="option">Perfil</div>
 			<div class="option">Opcions</div>
 			<div class="divisor_logout" ></div>
-			<div class="option">Log Out</div>
+			<div class="option"><a href="<?= base_url()?>index.php/logout">Log Out</a></div>
 		</div>
 		
 		<div id="sidebar">
+			<a href="<?= base_url()?>">
 			<div id="back">
 				<img src="<?= base_url()?>media/images/frontend/volver.png" />
-				<span><a href="<?= base_url()?>">TORNAR</a></span>
+				<span>TORNAR</span>
 			</div>
+			</a>
 			<div class="sidebar-option-none">
 			</div>
 			<div class="sidebar-option">
