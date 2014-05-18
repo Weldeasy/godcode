63378635b23ec659cb92e4ac9230f78bdf0ee061
oaoanton <oaoanton@gmail.com>
2014-05-18 23:44:29 +0200

    f

--- application/models/categorias.php
+++ application/models/categorias.php
@@ -3,7 +3,7 @@ Class Categorias extends CI_Model
 {
 	function get_categorias() {
 		$this->load->database();
-		$this->db->select('id, nom')->from('categoria');
+		$this->db->select('id, nom')->from('categoria_servei');
 		$query=$this->db->get();
 		return $query->result_array();
 	}
