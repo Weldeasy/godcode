a11d5ceaa8d61b85f672e68a5361be7e0682010f
wills1994 <wills1994@hotmail.com>
2014-05-19 21:10:51 +0200

    dd

--- application/views/frontend/panel_inici/info_usuari.php
+++ application/views/frontend/panel_inici/info_usuari.php
@@ -1,3 +1,38 @@
 <div class="infousuari">
-  
+<h1 class="label_title">Usuaris</h1>
+  <div class="un_info_usuari">
+  	<img src="<?=base_url()?>media/users_profile/thumbs/anonimo.jpg"/>
+  	<p>d</p>
+ 	<p>d</p> 	<p>d</p> 	<p>d</p>
+  </div>
+  <div class="un_info_usuari">
+  	<p>dd</p>
+  </div>
+  <div class="un_info_usuari">
+  	<p>dd</p>
+  </div>
+  <div class="un_info_usuari">
+  	<p>dd</p>
+  </div>
+  <div class="un_info_usuari">
+  	<p>dd</p>
+  </div>
+  <div class="un_info_usuari">
+  	<p>dd</p>
+  </div>
+  <div class="un_info_usuari">
+  	<p>dd</p>
+  </div>
+  <div class="un_info_usuari">
+  	<p>dd</p>
+  </div>
+  <div class="un_info_usuari">
+  	<p>dd</p>
+  </div>
+  <div class="un_info_usuari">
+  	<p>dd</p>
+  </div>
+   <div class="un_info_usuari">
+  	<p>1</p>
+  </div>
 </div>  
