98b5aebe76765e282fc57485eb63b6a2fb906393
wills1994 <wills1994@hotmail.com>
2014-05-19 19:56:21 +0200

    d

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -5,56 +5,45 @@ class Inicio extends CI_Controller {
   function __construct()
   {
     parent::__construct();
-  	$this->load->helper('url');
-  	$this->load->library('form_validation');
-  	$this->load->helper(array('form'));
-  	$this->load->model('categorias');
+    $this->load->helper('url');
+    $this->load->library('form_validation');
+    $this->load->helper(array('form'));
+    $this->load->model('categorias');
   }
 
   function index(){
-    	$data = array();
-    	
-    	$estat = $this->session->userdata('estat');
-    	if($this->session->userdata('logged_in')) {
-      		switch($estat) {
-      			case '1':
-      			case '2':
-      				$session_data = $this->session->userdata('logged_in');
-      				$data['email'] = $session_data['email'];
-      				$data['foto'] = $session_data['foto'];
-      				$this->load->view('frontend/logued', $data);
-      				break;
-      			case '3':
-      				$data['login_form'] = 'frontend/login_form';
-      				$data['contingut']=$this->load->view('frontend/panel_inici/congelat',null,TRUE);
-      				$this->load->view('frontend/inicio', $data);
-      			case '4':
-      				$data['login_form'] = 'frontend/login_form';
-      				$data['contingut']=$this->load->view('frontend/panel_inici/verifica',null,TRUE);
-      				$this->load->view('frontend/inicio', $data);
+      $data = array();
+      
+      $estat = $this->session->userdata('estat');
+      if($this->session->userdata('logged_in')) {
+          switch($estat) {
+            case '1':
+            case '2':
+              $session_data = $this->session->userdata('logged_in');
+              $data['email'] = $session_data['email'];
+              $data['foto'] = $session_data['foto'];
+              $this->load->view('frontend/logued', $data);
+              break;
+            case '3':
+              $data['login_form'] = 'frontend/login_form';
+              $data['contingut']=$this->load->view('frontend/panel_inici/congelat',null,TRUE);
+              $this->load->view('frontend/inicio', $data);
+            case '4':
+              $data['login_form'] = 'frontend/login_form';
+              $data['contingut']=$this->load->view('frontend/panel_inici/verifica',null,TRUE);
+              $this->load->view('frontend/inicio', $data);
 
-<<<<<<< HEAD
-      				break;
-      		}
+              break;
+          }
         }else{
-    		$data['categorias'] = $this->categorias->get_categorias();
-    		$data['login_form'] = 'frontend/login_form';
-    		$data['contingut']=$this->load->view('frontend/panel_inici/panel_principal',$data,TRUE);
-    		$this->load->view('frontend/inicio', $data);
-    	}
-=======
-				break;
-		}
-    }else{
-		$categorias = $this->categorias->get_categorias();
-		foreach($categorias as $row) {
-			$data['categorias'][$row['id']] = $row['nom'];
-		}
-		$data['login_form'] = 'frontend/login_form';
-		$data['contingut']=$this->load->view('frontend/panel_inici/panel_principal',$data,TRUE);
-		$this->load->view('frontend/inicio', $data);
-	}
->>>>>>> 3c1af08bbd666df4cde855b971b1388433b0ee96
+        $categorias = $this->categorias->get_categorias();
+      foreach($categorias as $row) {
+        $data['categorias'][$row['id']] = $row['nom'];
+      }
+    $data['login_form'] = 'frontend/login_form';
+    $data['contingut']=$this->load->view('frontend/panel_inici/panel_principal',$data,TRUE);
+    $this->load->view('frontend/inicio', $data);
+      }
   }
   function aboutus(){
       $data = array();
