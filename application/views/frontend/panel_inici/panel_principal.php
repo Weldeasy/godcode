83a0711cf8a5e828a880e7a94e727619c80a6a06
Weldeasy <vivaelcole@gmail.com>
2014-05-19 18:56:31 +0200

    aa

--- application/views/frontend/panel_inici/panel_principal.php
+++ application/views/frontend/panel_inici/panel_principal.php
@@ -12,8 +12,8 @@
 
                     <span role="status" aria-live="polite" class="ui-helper-hidden-accessible"></span><input id="autocomplete_city" name="autocomplete_city" class="input_ciudad ui-autocomplete-input" type="text" placeholder="Código postal ciudad: " required="" title="Por favor selecciona una ciudad" autofocus="" autocomplete="off">
                     <input id="date_from" class="input_fecha hasDatepicker" type="text" name="date_from" placeholder="" autocomplete="off" required="" title="Selecciona la fecha inicial de la reserva">
-                    <input id="date_to" class="input_fecha hasDatepicker" type="text" name="date_to" placeholder="" autocomplete="off" required="" title="Selecciona la fecha final de la reserva">
-					<?php echo form_dropdown($categorias); ?>
+                    <!--<input id="date_to" class="input_fecha hasDatepicker" type="text" name="date_to" placeholder="" autocomplete="off" required="" title="Selecciona la fecha final de la reserva">-->
+					<?php echo form_dropdown('categorias', $categorias); ?>
                     <input type="submit" value="Buscar servicios" class="bt_ver_coches">
                 </form>
               </div>
