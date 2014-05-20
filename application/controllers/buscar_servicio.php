e574d5901550506aaeccd834923579b675cbc5fc
Weldeasy <vivaelcole@gmail.com>
2014-05-20 13:47:05 +0200

    aa

---
+++ application/controllers/buscar_servicio.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Buscar_servicio extends CI_Controller {
+
+
+
+  function index()
+  {
+    echo "buscar_servicio";
+  }
+?>
