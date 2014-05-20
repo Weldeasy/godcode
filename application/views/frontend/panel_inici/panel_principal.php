421486596fe550ca0b245df4dccc8b061f330c8c
Weldeasy <vivaelcole@gmail.com>
2014-05-20 13:28:12 +0200

    aa

--- application/views/frontend/panel_inici/panel_principal.php
+++ application/views/frontend/panel_inici/panel_principal.php
@@ -14,11 +14,7 @@
 					<input id="autocomplete_city" name="autocomplete_city" class="input_ciudad ui-autocomplete-input" type="text" placeholder="Código postal ciudad: " required="" title="Por favor selecciona una ciudad" autofocus="" autocomplete="off">
 					<input type="text" name="datainici" id="datepicker1" placeholder="Serveis disponibles desde: ">
 					<input type="text" name="datafi" id="datepicker2" placeholder="Serveis disponibles fins el: ">
-<<<<<<< HEAD
 					<?php echo form_dropdown('categorias', $categorias, 'id="datepicker1"'); ?>
-=======
-					<?php echo form_dropdown('categorias', $categorias, 'name="categoria"'); ?>
->>>>>>> e6b3b036767544aa0b122df649013ae66ed65a96
                     <input type="submit" value="Buscar servicios" class="bt_ver_coches">
                 </form>
               </div>
