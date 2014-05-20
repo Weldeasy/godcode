45e925d6f86c6f1ff2ac7748795cec0e8d64de41
amartintapia <alejandromartintapia@gmail.com>
2014-05-20 16:37:18 +0200

    date

--- application/views/frontend/panel_inici/panel_principal.php
+++ application/views/frontend/panel_inici/panel_principal.php
@@ -9,9 +9,9 @@
               <br>
               <div class="contenido_formulario_busqueda">
                 <form id="landingForm" action="<?php echo base_url(); ?>index.php/buscar_servicio" method="post">
-          					<input id="city" name="autocomplete_city" class="input_ciudad ui-autocomplete-input" type="text" placeholder="Códi postal o nom de la ciutat: " title="Per favor selecciona una ciutat" autofocus="" >
-          					<input type="text" name="datainici" id="datepicker1" placeholder="disponibles desde: ">
-          					<input type="text" name="datafi" id="datepicker2" placeholder="disponibles fins el: ">
+          					<input id="city" name="city" class="input_ciudad ui-autocomplete-input" type="text" placeholder="Códi postal o nom de la ciutat: " title="Per favor selecciona una ciutat" autofocus="" >
+          					<input type="date" name="datainici" id="datepicker1" placeholder="disponibles desde: ">
+          					<input type="date" name="datafi" id="datepicker2" placeholder="disponibles fins el: ">
           					<?php echo form_dropdown('categorias', $categorias, 'name="categories" id="categorias"'); ?>
                     <input type="submit" value="Buscar servicios" class="bt_ver_coches">
                 </form>
