b8da844dff2ed3bd28c586d825d6dbb33b48f609
wills1994 <wills1994@hotmail.com>
2014-05-21 17:02:27 +0200

    d

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -54,7 +54,87 @@ class Inicio extends CI_Controller {
       		$data['contingut']=$this->load->view('frontend/panel_inici/panel_principal',$data,TRUE);
       		$this->load->view('frontend/inicio', $data);
       	}
+
   }
+    public function aboutus() {
+      $data = array();
+        if($this->session->userdata('logged_in')) {
+          $session_data = $this->session->userdata('logged_in');
+          $data['foto'] = $session_data['foto'];
+          $data['email'] = $session_data['email'];
+          $data['login_form'] = 'frontend/panel_inici/logued';
+        }else{
+          $data['login_form'] = 'frontend/login_form';
+        }
+        $data['contingut']=$this->load->view('frontend/panel_inici/aboutus',$data,TRUE);
+        $this->load->view('frontend/inicio', $data);
+    }
+    public function cercarusuari() {
+      $data = array();
+        if($this->session->userdata('logged_in')) {
+            $session_data = $this->session->userdata('logged_in');
+           $data['foto'] = $session_data['foto'];
+          $data['email'] = $session_data['email'];
+          $data['login_form'] = 'frontend/panel_inici/logued';
+        }else{
+            $data['login_form'] = 'frontend/login_form';
+        }
+        $data['contingut']=$this->load->view('frontend/panel_inici/cercarusuari',$data,TRUE);
+        $this->load->view('frontend/inicio', $data);
+  }
+  public function contacte() {
+        $data = array();
+        if($this->session->userdata('logged_in')) {
+          $session_data = $this->session->userdata('logged_in');
+         $data['foto'] = $session_data['foto'];
+          $data['email'] = $session_data['email'];
+          $data['login_form'] = 'frontend/panel_inici/logued';
+        } else {
+          $data['login_form'] = 'frontend/login_form';
+        }
+        $data['contingut']=$this->load->view('frontend/panel_inici/contacte',$data,TRUE);
+        $this->load->view('frontend/inicio', $data);
+  
+  }
+  function detailusuari() {
+      if(isset($_POST['cercar_user'])){
+        $cercar_user = $_POST['cercar_user'];  
+
+        if($this->session->userdata('logged_in')) {
+            $session_data = $this->session->userdata('logged_in');
+            $data['foto'] = $session_data['foto'];
+            $data['email'] = $session_data['email'];
+            $data['login_form'] = 'frontend/panel_inici/logued';
+        }else{
+            $data['login_form'] = 'frontend/login_form';
+        }
+        
+        $data['users']=$this->user->cercar_user_servei($cercar_user);
+        $data['contingut']=$this->load->view('frontend/panel_inici/detailusuari',$data);
+        $this->load->view('frontend/inicio', $data);
+      }else{
+        echo "no existe";
+      }
+  }
+  public function introduccio() {
+    $data = array();
+      if($this->session->userdata('logged_in')) {
+        $session_data = $this->session->userdata('logged_in');
+        $data['foto'] = $session_data['foto'];
+      $data['email'] = $session_data['email'];
+      $data['login_form'] = 'frontend/panel_inici/logued';
+      }else{
+        $data['login_form'] = 'frontend/login_form';
+      }
+      $data['contingut']=$this->load->view('frontend/panel_inici/introduccio',$data,TRUE);
+      $this->load->view('frontend/inicio', $data);
+  }
+  public function no_autentificat() {
+    $data = array();
+    $data['login_form'] = 'frontend/login_form';
+    $this->load->view('frontend/panel_inici/no_autentificat',$data);
+  }
+
 }
 
 ?>
--- application/views/frontend/inicio.php
+++ application/views/frontend/inicio.php
@@ -25,10 +25,10 @@
  <div id='cssmenu'>
 	<ul>
 		<li class='active'><a href='<?= base_url()?>'><span>INICI</span></a></li>
-		<li class='active'><a href='<?= base_url()?>index.php/introduccio'><span>QUÉ ES UN BANCO DEL TIEMPO?</span></a></li>
-    <li class='active'><a href='<?= base_url()?>index.php/cercarusuari'><span>Usuaris</span></a></li>
-		<li class='active'><a href='<?= base_url()?>index.php/aboutus/'><span>SOBRE GODCODE</span></a></li>
-		<li class='active'><a href='<?= base_url()?>index.php/contacte/'><span>CONTACTA</span></a></li>
+		<li class='active'><a href='<?= base_url()?>index.php/inicio/introduccio'><span>QUÉ ES UN BANCO DEL TIEMPO?</span></a></li>
+    <li class='active'><a href='<?= base_url()?>index.php/inicio/cercarusuari'><span>Usuaris</span></a></li>
+		<li class='active'><a href='<?= base_url()?>index.php/inicio/aboutus/'><span>SOBRE GODCODE</span></a></li>
+		<li class='active'><a href='<?= base_url()?>index.php/inicio/contacte/'><span>CONTACTA</span></a></li>
     <li class='active'><a href='<?= base_url()?>index.php/formularioregistro/'><span>REGISTRA'T</span></a></li>
     <?php echo $this->load->view($login_form); ?>
   </ul>
