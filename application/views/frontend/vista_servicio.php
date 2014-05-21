08881c455b586580880e7eeefa416589b1357470
oaoanton <oaoanton@gmail.com>
2014-05-21 17:08:25 +0200

    f

--- application/views/frontend/vista_servicio.php
+++ application/views/frontend/vista_servicio.php
@@ -4,13 +4,19 @@
 	</div>
 	<div class="cuerpo_servicio">
 		<div class="titulo_servicio">
-			<?=$nom?>
+			<span class="nom_servei"><?=$nom?></span>
+			<span class="data_caducitat">Data caducitat: <?=$data_fi?></span>
 		</div>
 		<div class="descripcion_servicio">
 			<?=$descripcio?>
 			<div class="precio_servicio">
-				<?=$preu?>
+				<?=$preu?> punts
 			</div>
 		</div>
+	</div>
+	<div class="servei_actions">
+		<input type="button" value="Editar" />
+		<input type="button" value="Eliminar" />
+		<input type="button" value="Congelar" />
 	</div>
 </div>
