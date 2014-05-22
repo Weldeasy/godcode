32df96da27d2ccc8406ba304b86038a98527595b
Weldeasy <vivaelcole@gmail.com>
2014-05-22 17:51:45 +0200

    aa

--- application/views/frontend/resultado_servicios.php
+++ application/views/frontend/resultado_servicios.php
@@ -23,7 +23,7 @@
     <li class='active'><a href='<?= base_url()?>index.php/formularioregistro/'><span>Registre</span></a></li>
     <li class='active'><a href='<?= base_url()?>index.php/inicio/contacte/'><span>Contacte</span></a></li>
     
-    <?php echo $this->load->view($login_form, null); ?>
+    <?php echo $this->load->view($login_form, null, true); ?>
   </ul>
  </div>
  <div id="contenido_principal">
