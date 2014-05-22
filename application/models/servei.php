d4e4141e3c0dfe4d89781b9a634a7f48984f5106
Weldeasy <vivaelcole@gmail.com>
2014-05-22 16:34:49 +0200

    aa

--- application/models/servei.php
+++ application/models/servei.php
@@ -69,7 +69,7 @@ Class Servei extends CI_Model {
 			//$this -> db -> where('s.data_fi >= '.$dataFi);
 			$sql .= 'DATEDIFF( s.data_fi, '.$dataFi.') <=0';
 		}
-		$query = $this->db->query($sql);
+		//$query = $this->db->query($sql);
 
 		//$query = $this -> db -> get();
 		//return $query->result();
