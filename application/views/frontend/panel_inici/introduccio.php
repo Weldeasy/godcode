df42872be3fba9370a5e69f24f87602ee90b6f24
amartintapia <alejandromartintapia@gmail.com>
2014-05-22 19:00:50 +0200

    ada

--- application/views/frontend/panel_inici/introduccio.php
+++ application/views/frontend/panel_inici/introduccio.php
@@ -3,6 +3,6 @@
               <h1>INTRODUCCIÓ</h1>
               <hr />
               <br />
-              <p>Podem definir el banc de temps com intercanvis dels serveis entre usuaris.</p>
+              <p>Un banc de temps és un sistema d'intercanvi de serveis per temps. En ell la unitat d'intercanvi no és els diners habituals sinó una mesura de temps, per exemple el treball per hora. És un sistema d'intercanvi de serveis per serveis o favors per favors. Proposa l'avantatge de fomentar les relacions socials i la igualtat entre diferents estrats econòmics. Es planteja l'ús d'aquest tipus d'economia per solucionar diversos problemes presents en l'economia de mercat, a manera d'economies complementàries o mercats alternatius. Actualment aquests projectes poden ser potenciats amb el suport de la tecnologia de la informació.</p>
   </div>
 </div>
