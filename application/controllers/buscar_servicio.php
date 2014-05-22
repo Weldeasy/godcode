7fdfe82dcd401cc4e38dab7c884d74e5d98aaab9
Weldeasy <vivaelcole@gmail.com>
2014-05-22 17:48:29 +0200

    aa

--- application/controllers/buscar_servicio.php
+++ application/controllers/buscar_servicio.php
@@ -13,7 +13,7 @@ class Buscar_servicio extends CI_Controller {
 		$serveis = $this->buscar();
 		$html = "";
 
-		foreach($serveis as $row) {
+		/*foreach($serveis as $row) {
 			$data2 = array(
 			  'id' => $row->id,
 			  'nom' => $row->nom,
@@ -32,7 +32,7 @@ class Buscar_servicio extends CI_Controller {
 	
 	
 		$data['html'] = $html;
-		$data['login_form'] = 'frontend/login_form';
+		$data['login_form'] = 'frontend/login_form';*/
 		/*if($this->session->userdata('logged_in')) {
 			$data['login_form'] = 'frontend/panel_inici/logued';
 			$session_data = $this->session->userdata('logged_in');
