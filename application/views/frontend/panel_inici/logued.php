22ef83b29db37bfe8eb86e41f99de3562aa81f24
amartintapia <alejandromartintapia@gmail.com>
2014-05-22 18:48:49 +0200

    admin bueno

--- application/views/frontend/panel_inici/logued.php
+++ application/views/frontend/panel_inici/logued.php
@@ -1,7 +1,11 @@
- <?php echo ($es_admin); ?>
+
+
  <li class="welcome_msg" id="redondo"><img id="userfoto" src="<?php echo base_url().'media/users_profile/thumbs/'.$foto; ?>" /></li>
      <li class="welcome_msg"><?php echo "Hola,  ".$email; ?></li>
     <br/>
     <a class="logout_b" title="Tancar sessio" href="<?= base_url()?>index.php/logout"><img src="<?= base_url()?>media/images/frontend/close_session.png" /></a>
+     <?php if($es_admin ==1){?>
+     	<a class="user_settings" title="La meva conte" href="<?= base_url()?>index.php/admin"><img src="<?=base_url()?>media/images/frontend/user_settings.png" /></a>
+    <?php }else{ ?>
     <a class="user_settings" title="La meva conte" href="<?= base_url()?>index.php/user_settings"><img src="<?=base_url()?>media/images/frontend/user_settings.png" /></a>
- 
+ 	<?php } ?>
