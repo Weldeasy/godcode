cf600719da98f58e9a07ce746fe3a08480cb1f06
wills1994 <wills1994@hotmail.com>
2014-05-23 20:48:15 +0200

    dd

--- application/views/frontend/inicio.php
+++ application/views/frontend/inicio.php
@@ -12,7 +12,6 @@
 	 <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
 	<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-	<link rel="stylesheet" href="/resources/demos/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<script>
 	$(function() {
