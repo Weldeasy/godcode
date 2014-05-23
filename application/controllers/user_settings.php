914c146030856c246c04c828879cd80222db1518
oaoanton <oaoanton@gmail.com>
2014-05-23 16:54:21 +0200

    g

--- application/controllers/user_settings.php
+++ application/controllers/user_settings.php
@@ -154,7 +154,7 @@ class User_settings extends CI_Controller {
 		$disponibilitat_horaria = $this->input->post("hores");
 		$disponibilitat_dies = "";
 		foreach ($this->input->post("days") as $key => $value) {
-			$disponibilitat_dies = $disponibilitat_dies.$this->input->post("days")[$key].";";
+			echo $this->input->post("days")[$key].";";
 		}
 		
 	}
