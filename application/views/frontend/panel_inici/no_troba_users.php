60ea2ae7a3333459d1aa9425c774f75d40d3bf7d
wills1994 <wills1994@hotmail.com>
2014-05-23 20:37:50 +0200

    s

--- application/views/frontend/panel_inici/no_troba_users.php
+++ application/views/frontend/panel_inici/no_troba_users.php
@@ -1,6 +1,6 @@
 <div class="infousuari">
 	<h1 class="label_title">Serveis dels usuaris</h1>
 	<div class='un_info_usuari'>
-            <p>No s'ha trobat cap usuari buscat</p>
+            <p>No s'ha trobat cap usuari buscats</p>
     </div>
 </div>
