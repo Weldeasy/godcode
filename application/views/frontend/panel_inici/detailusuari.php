8907b8fc1236473a69f826b501a08c3f15f57fad
wills1994 <wills1994@hotmail.com>
2014-05-23 19:54:30 +0200

    VISTA USUARI

--- application/views/frontend/panel_inici/detailusuari.php
+++ application/views/frontend/panel_inici/detailusuari.php
@@ -1,27 +1,19 @@
-<div class="infousuari">
-  <h1 class="label_title">Serveis dels usuaris</h1>
-  <?php 
-        if($users!=null){
-            foreach ($users as $data) { 
-        ?>
-              <div class="un_info_usuari">
-                <form id='formulari' method="post" action="<?=base_url()?>index.php/inicio/serveis_detail">
-                  <p><b>Nom i cognom:</b><?php echo $data->nom_usuari." ".$data->cognom; ?></p>
-                  <?php if($data->foto!=null){?>
-                    <img src='<?=base_url() ?>media/users_profile/thumbs/<?= $data->foto ?>.jpg' />
-                  <?php }else{ ?>
-                    <img src='<?=base_url()?>media/users_profile/thumbs/anonimo.jpg'/>
-                  <?php } ?>
-                  <p><b>Correu :</b><?php echo $data->email; ?></p>
-                  <p><b>Població :</b><?php echo $data->poblacion; ?></p>
-                  <input type='hidden' value='<?php echo $data->email; ?>' name='email_user'/>
-                  <input type='submit' value='Veure Serveis' class="buttonform" />
-                </form>
-              </div>
-  <?php   } 
-     }else{?>
-          <div class='un_info_usuari'>
-             <p>No s'ha trobat cap usuari buscat</p>
-          </div>
-     <?php } ?>
+<div class="un_info_usuari">
+  <form id='formularidetailuser' method="post" action="<?=base_url()?>index.php/inicio/serveis_detail">
+          <?php if($foto!=null){?>
+              <img src='<?=base_url() ?>media/users_profile/thumbs/<?= $foto ?>' />
+          <?php }else{ ?>
+              <img src='<?=base_url()?>media/users_profile/thumbs/anonimo.jpg'/>
+          <?php } ?>
+            <div class="text_info_user">
+               <b>Nom i cognoms :</b><?= $nom_usuari." ".$cognom; ?><br />
+               <b>Correu :</b><?= $email; ?><br />
+               <b>Pobalció :</b><?= $poblacion; ?>
+            </div>
+            <div class='canviposicio'>    
+                <input type='hidden' value='<?= $email; ?>' name='email_user'/>
+                <input type='submit' value='Veure Serveis' class="buttonform" />
+            </div>
+  </form>
 </div>
+
