740918f94454869b503258f4f8513317880d0aaf
Weldeasy <vivaelcole@gmail.com>
2014-05-24 23:24:18 +0200

    aa

--- application/views/frontend/user_settings/alta_servei.php
+++ application/views/frontend/user_settings/alta_servei.php
@@ -64,7 +64,7 @@
 			<a href="<?= base_url()?>index.php/user_settings/serveis"><div class="sidebar-option">SERVEIS</div></a>
 			<a><div class="sidebar-option">SOLICITUDS</div></a>
 		</div>
-
+			aaas
 		<div id="page-wrapper">
 		</div>
 	</body>
