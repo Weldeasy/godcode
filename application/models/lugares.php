4c9257e22362785f3fa0e9282fb747cc87610ba3
Weldeasy <vivaelcole@gmail.com>
2014-05-25 17:46:26 +0200

    aa

--- application/models/lugares.php
+++ application/models/lugares.php
@@ -2,12 +2,8 @@
 Class Lugares extends CI_Model
 {
 	function get_poblacion_by_cp($cp) {
-		$this->load->database();
-		$this->db->select('*');
-		$this->db->from('poblacion');
-		$this->db->where('postal', $cp);
-		$query=$this->db->get();
-		return $query->result_array();
+		$query = $this->db->query('SELECT * FROM poblacion WHERE postal = '.$cp.' LIMIT 1');
+		return $query->row();
 	}
 }
 ?>
