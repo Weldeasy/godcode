33308d711b5f56ca6cccb6c4937fc12fcae2e9f3
Weldeasy <vivaelcole@gmail.com>
2014-05-25 02:33:09 +0200

    aa

--- application/views/frontend/user_settings/alta_servei.php
+++ application/views/frontend/user_settings/alta_servei.php
@@ -15,9 +15,8 @@
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 		<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
-		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-		<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-		<link rel="stylesheet" href="/resources/demos/style.css">
+		<script src="code.jquery.com/jquery-1.10.2.js"></script>
+		<script src="code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 		
 		<script>
 		window.onload = function(){
