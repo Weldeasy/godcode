8e9f35347ea2f2443e23535833700a595a89473f
Weldeasy <vivaelcole@gmail.com>
2014-05-25 03:49:23 +0200

    aa

--- application/models/servei.php
+++ application/models/servei.php
@@ -50,7 +50,7 @@ Class Servei extends CI_Model {
 			"disp_horaria"=>$disponibilitat_horaria,
 			"disp_dies"=>$disponibilitat_dies,
 			"categoria"=>$this->input->post("categoria", TRUE),
-			"usuari"=>$this->session->userdata('logged_in')['id'],
+			"usuari"=>$this->input->post("usuari", TRUE),
 			"cp"=>$this->input->post("cp", TRUE)
 		));
 		return true;
