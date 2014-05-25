51d809bb9b0f87110dcf02f4284119f374468d37
Weldeasy <vivaelcole@gmail.com>
2014-05-25 02:36:51 +0200

    aa

--- application/views/frontend/user_settings/alta_servei.php
+++ application/views/frontend/user_settings/alta_servei.php
@@ -14,7 +14,7 @@
 		<script src="<?= base_url()?>media/js/ion-rangeSlider/ion.rangeSlider.min.js"></script>
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 		<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-		<script src="code.jquery.com/jquery-1.10.2.js"></script>
+		<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
 		
 		<script>
 		window.onload = function(){
