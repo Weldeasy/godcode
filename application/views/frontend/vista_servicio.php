93927c1899e053de57584231a417787df2327583
Weldeasy <vivaelcole@gmail.com>
2014-05-25 16:04:15 +0200

    aa

--- application/views/frontend/vista_servicio.php
+++ application/views/frontend/vista_servicio.php
@@ -1,3 +1,4 @@
+<?php /*
 <div class="servicio">
 	<div class="foto_servicio">
 		<img src="<?=base_url()?>media/images/categorias/<?=$categoria?>.jpg" />
@@ -18,5 +19,27 @@
 		<a href="<?=base_url()?>index.php/user_settings/editar_servei/<?=$id?>"><input type="button" value="Editar" /></a>
 		<input type="button" value="Eliminar" />
 		<input type="button" value="Congelar" />
+	</div>
+</div>*/ 
+?>
+
+<div class="servicio">
+	<div class="foto_servicio">
+		<img src="<?=base_url()?>media/images/categorias/<?=$categoria?>.jpg" />
+	</div>
+	<div class="cuerpo_servicio">
+		<div class="titulo_servicio">
+			Titulo del servicio
+		</div>
+		<div class="descripcion_servicio">
+			Descripcion del servicio, etc, bla bla bla, mi coche es de color
+			del amor en pleno mes de abril
+		</div>
+		<div class="precio_servicio">
+			301
+		</div>
+		<div class="dispo_servicio">
+			
+		</div>
 	</div>
 </div>
