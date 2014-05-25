3c211ebe3635078cd7708a7105e67d130ad94ac2
Weldeasy <vivaelcole@gmail.com>
2014-05-25 04:30:46 +0200

    aa

--- application/controllers/user_settings.php
+++ application/controllers/user_settings.php
@@ -214,9 +214,14 @@ class User_settings extends CI_Controller {
   }
   
   function editar_servei($id, $missatge = null) {
-		
+		$session_data = $this->session->userdata('logged_in');
 		$servicio = $this->servei->get_servei($id);
-		var_dump($servicio);
+		
+		if ($servicio['usuari'] == $session_data['id']) {
+			echo "si";
+		} else {
+			echo "no";
+		}
 		
 		
 		/*$this->db->select('*');
