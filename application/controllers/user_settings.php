da77e3aa7deeea0d8064c20c22655085c63aac64
Weldeasy <vivaelcole@gmail.com>
2014-05-25 03:19:33 +0200

    aa

--- application/controllers/user_settings.php
+++ application/controllers/user_settings.php
@@ -152,19 +152,14 @@ class User_settings extends CI_Controller {
 		$data_inici = date_create(date('Y-m-d'));
 		$diff = date_diff($data_fi,$data_inici)->format('%R%a');
 		if ($valida) {
-			/*if (strrpos($diff, "-") === false) {
-				$this->form_validation->set_message('dataFi_check', '%s debe ser una fecha posterior a hoy');
-				return false;
-			} else {
-				return true;
-			}*/
+			
 			return true;
 		} else {
-			$this->form_validation->set_message('dataFi_check', '%s es una fecha NO valida');
+			$this->form_validation->set_message('data_check', '%s es una fecha NO valida');
 			return false;
 		}
 	} else {
-		$this->form_validation->set_message('dataFi_check', '%s no es ni una fecha<br>[2015-06-25]');
+		$this->form_validation->set_message('data_check', '%s no es ni una fecha<br>[2015-06-25]');
 		return false;
 	}
 	}
@@ -202,7 +197,7 @@ class User_settings extends CI_Controller {
 		}
 		$data_inici = date('Y-m-d');
 		$this->servei->add_servei($data_inici, $disponibilitat_horaria, $disponibilitat_dies);
-		redirect('user_settings/serveis','refresh');
+		//redirect('user_settings/serveis','refresh');
 	}
 	
 	
