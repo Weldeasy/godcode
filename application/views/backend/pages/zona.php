e549c4c2fa00719de974fbf010fe28c47386b819
amartintapia <alejandromartintapia@gmail.com>
2014-05-26 19:56:33 +0200

    zona

--- application/views/backend/pages/zona.php
+++ application/views/backend/pages/zona.php
@@ -1,11 +1,4 @@
-<?php echo "<h1>Zona geografica</h1>" 
-
-echo $this->gcharts->LineChart('Stocks')->outputInto('stock_div');
-echo $this->gcharts->div(600, 300);
-
-if($this->gcharts->hasErrors())
-{
-    echo $this->gcharts->getErrors();
-}
-
+<?php 
+  echo $this->gcharts->ColumnChart('Population')->outputInto('inventori_div');
+  echo $this->gcharts->div(600,500);
 ?>
