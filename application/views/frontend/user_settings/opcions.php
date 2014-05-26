9344d3bda2bbf2e0cba31cba2b76242299f9e4b2
wills1994 <wills1994@hotmail.com>
2014-05-26 14:41:11 +0200

    ss

--- application/views/frontend/user_settings/opcions.php
+++ application/views/frontend/user_settings/opcions.php
@@ -8,3 +8,14 @@
 		</tr>
 	</table>
 </form>
+<hr />
+<form id='formulariUser' method="post" action="<?=base_url()?>index.php/user_settings/donarBaixaUser_control">
+	<table>
+		<tr>
+			<td>
+				<input type="hidden" value="<?= $email ?>" name='email'/>
+            	<input type='submit' value='Canvia Contrasenya' name='baixa' class="buttonform" />
+			</td>
+		</tr>
+	</table>
+</form>
