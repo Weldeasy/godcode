40a1a5bfeb6cb086a0359de5f5d38a8772d3f8c5
Weldeasy <vivaelcole@gmail.com>
2014-05-27 16:46:32 +0200

    aa

--- application/controllers/scripts/sistema.php
+++ application/controllers/scripts/sistema.php
@@ -8,19 +8,32 @@ class Sistema extends CI_Controller {
 		$this->load->model('servei');
 		$this->load->model('administrador');
 	}
-
+	
+	
+	/**
+	* Este metodo congela los servicios que no se han consumido nunca pasado cierto tiempo.
+	* Tambien congela los servicios que han superado la data_fi
+	*/
 	function congelar_serveis() {
 		$servicios = $this->servei->get_serveis_noConsumit(null);
+		$all_servicios = $this->servei->get_serveis(null);
 		$max_dies_congetal = $this->administrador->getMax_dies_congelat()->max_dias_congelado;
 		$data_actual = date("Y-m-d");
 		echo "<pre>";
 		foreach($servicios as $servicio) {
 			$data_inici = date($servicio->data_inici);
-			$diff = date_diff(date_create($data_inici), date_create($data_actual))->days;
-			var_dump($diff);
+			$diff_noConsumits = date_diff(date_create($data_inici), date_create($data_actual))->days;
+		}
+		
+		foreach($all_servicios as $servicio) {
+			$data_fi = date($servicio->data_fi);
+			$diff_dataFi = date_diff(date_create($data_fi), date_create($data_actual))->days;
+			var_dump($diff_dataFi);
 		}
 		echo "</pre>";
 	}
+	
+	
 
 }
 
