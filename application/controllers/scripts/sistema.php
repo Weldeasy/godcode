752a4854d5d4412367ca3de7c313cf677d11d2ef
Weldeasy <vivaelcole@gmail.com>
2014-05-28 02:30:31 +0200

    aaa

--- application/controllers/scripts/sistema.php
+++ application/controllers/scripts/sistema.php
@@ -31,7 +31,7 @@ class Sistema extends CI_Controller {
 		foreach($servicios as $servicio) {
 			$data_inici = strtotime(date($servicio->data_inici));
 			$diff = ($data_actual - $data_inici)/3600/24;
-			echo $$data_actual - $data_inici;
+			echo $data_actual - $data_inici;
 			if ($diff>=$max_dies_noConsumit) {
 				//$this->servei->congelarServei($servicio->id);
 				
