52484859590fc2d49ab639bb200d4b5bff775b08
wills1994 <wills1994@hotmail.com>
2014-05-28 16:46:24 +0200

    d

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -21,9 +21,9 @@ class Inicio extends CI_Controller {
     $this->data['email'] = $session_data['email'];
     $this->data['foto'] = $session_data['foto'];
     $this->data['es_admin'] = $session_data['es_admin'];
-    
-    $this->data['saldo']=$this->user->getSaldoUser($session_data['email'])->saldo;
-
+    if($session_data){
+      $this->data['saldo']=$this->user->getSaldoUser($session_data['email'])->saldo;
+    }  
   }
   /**
    * [contingut es carrega la pagina principal, amb les vistes i les dades]
