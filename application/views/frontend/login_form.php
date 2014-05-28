2d7eae1de9293256b4e7a7ee8d0ec60c417eed2b
oaoanton <oaoanton@gmail.com>
2014-05-28 18:35:01 +0200

    f

--- application/views/frontend/login_form.php
+++ application/views/frontend/login_form.php
@@ -9,7 +9,7 @@ echo form_open('verifylogin',array('class'=>'login_form'));
 </li>
 <div id="login_errors">
 	<ul>
-		<li><?php if (isset($this->session->userdata)) print $this->session->email_login;?></li>
+		<li><?php if (isset($this->session->userdata('logine')->email_login)) print $this->session->userdata('logine')->email_login;?></li>
 		<li><?php $password_login?></li>
 	</ul>
 </div>
