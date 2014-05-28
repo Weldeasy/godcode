fa061887489438044423fe10cedaa73e5ea70821
Weldeasy <vivaelcole@gmail.com>
2014-05-28 03:29:40 +0200

    aa

--- application/views/frontend/vista_servicio.php
+++ application/views/frontend/vista_servicio.php
@@ -33,6 +33,17 @@
 					<td <?php if ( (isset($days)) && (!in_array('D', $days)) ) print 'style="background-color:#FA5858;"'; ?> value="L" >D</td>
 				</tr>
 			</table>
+			<div class="servei_actions">
+			<?php 
+              /*Surtirà butò si usuari està loguejat i el user no pot sol·licitar si mateix i no sigui admin*/
+              if(isset($email) && ($user_oferit_servei!=$email) && ($es_admin==0)){
+          ?>
+          <td>
+              <input type='submit' value='Solicitar' onclick="solicitut()" class="buttonform"/>
+          </td>
+
+          <?php } ?>
+		</div>
 	</div>
 	<div id="formSolicitut" class="easyui-dialog" title="Missatge" data-options="iconCls:'icon-save'" closed="true" style="width:400px;height:200px;padding:10px">
       <form id='formulariservei' method='post' action="<?=base_url()?>index.php/inicio/verifica_solicitut">
