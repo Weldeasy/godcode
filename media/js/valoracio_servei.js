f1ce80d33edce286648baa55a25ec32898379467
oaoanton <oaoanton@gmail.com>
2014-06-04 23:22:30 +0200

    1

--- media/js/valoracio_servei.js
+++ media/js/valoracio_servei.js
@@ -1,8 +1,10 @@
-$("#consumirBtn").click(function () {
-	alert($("#valoracio").val());
-	if ($("#valoracio").val() != 0) {
-		$("#formularisolicitud").submit();
-	} else {
-		alert("Has de valorar el servei.");
-	}
+$( document ).ready(function() {
+	$("#consumirBtn").click(function () {
+		alert($("#valoracio").val());
+		if ($("#valoracio").val() != 0) {
+			$("#formularisolicitud").submit();
+		} else {
+			alert("Has de valorar el servei.");
+		}
+	});
 });
