ba8c515d8e36b3148447b9c9d6e3d547c90c483e
oaoanton <oaoanton@gmail.com>
2014-06-04 23:02:02 +0200

    valoracio1

--- application/views/frontend/user_settings/llistatConsumirServei.php
+++ application/views/frontend/user_settings/llistatConsumirServei.php
@@ -2,12 +2,13 @@
 	<form id='formularisolicitud' method="post" action="<?=base_url()?>index.php/user_settings/consumit">
 		<table>
 			<tr>
-				<td><b>Nom del Servei:</b> <?= utf8_decode($servei_nom) ?><br />
+				<td><b>Servei:</b> <?= utf8_decode($servei_nom) ?><br />
 				<b>Preu:</b> <?= $preu ?><br />
-				<b>Data de fi:</b><?= utf8_decode($data_fi) ?><br />
+				<b>Data expiraci&oacute;:</b><?= utf8_decode($data_fi) ?><br />
 				<input type="hidden" value="<?= $usuari ?>" name='id_usuari_servei'/>
 				<input type="hidden" value="<?= $id_consumidor ?>" name='id_consumidor'/>
 				<input type="hidden" value="<?= $id_servei ?>" name='id_servei'/>
+				<b>Valora el servei</b>&nbsp;<select name="valoracio" id="valoracio"><option value="0">Selecciona opcio..</option><option value="1">Molt bé</option><option value="2">B&eacute;</option><option value="3">Molt Malament</option></select>
                 <input type='submit' value='Consumir' name='consumir' class="buttonform" />
 				</td>
 			</tr>
